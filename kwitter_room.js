e98b7401f252d886902fda332d12c1f0ff0e796f
HU-M <83540224+HU-M@users.noreply.github.com>
2021-08-21 15:10:55 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAuc670uJ6WWfno_p180mEEbJZa_JF3cGQ",
+    authDomain: "k-witter-70f22.firebaseapp.com",
+    databaseURL: "https://k-witter-70f22-default-rtdb.firebaseio.com",
+    projectId: "k-witter-70f22",
+    storageBucket: "k-witter-70f22.appspot.com",
+    messagingSenderId: "780516095759",
+    appId: "1:780516095759:web:5bf1745d1be7160114b07a",
+    measurementId: "G-MRZ2ZCJL9Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
